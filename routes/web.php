6503ca6aa407042c7f4cb68a887f28557a0f2ac3
DanU-R <lembing060@gmail.com>
2026-05-21 22:27:51 +0700

    Update dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MenuDescriptionController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PosController;
@@ -10,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::get('dashboard', DashboardController::class)->name('dashboard');
 
     Route::get('ai/menu-description', [MenuDescriptionController::class, 'edit'])
         ->name('menu-description.edit');
